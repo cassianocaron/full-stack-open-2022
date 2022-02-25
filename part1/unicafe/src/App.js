d779062cffa648443108cdbd58bf012c2842d6cf
Cassiano Caron <cassianocaron@hotmail.com>
2022-02-25 16:37:21 -0300

    Unicafe step1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default App
