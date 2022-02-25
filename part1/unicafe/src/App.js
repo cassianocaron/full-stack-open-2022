45a0eb54d76dac363e285a6188802ea6fc4bbdc3
Cassiano Caron <cassianocaron@hotmail.com>
2022-02-25 18:26:38 -0300

    Unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,11 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
+const Statistics = ({ text, value }) => (
+    <div>{text} {value}</div>
+  )
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -19,12 +24,12 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <h1>statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {(good - bad) / all}</div>
-      <div>positive {(good / all) * 100} %</div>
+      <Statistics text='good' value={good} />
+      <Statistics text='neutral' value={neutral} />
+      <Statistics text='bad' value={bad} />
+      <Statistics text='all' value={all} />
+      <Statistics text='average' value={(good - bad) / all} />
+      <Statistics text='positive' value={(good / all) * 100} />
     </div>
   )
 }
