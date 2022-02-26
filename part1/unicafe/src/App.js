1a0ae9431239ec38aea6e6feb29d68b129828a21
Cassiano Caron <cassianocaron@hotmail.com>
2022-02-26 09:21:11 -0300

    Unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,8 +5,8 @@ const Button = ({ handleClick, text }) => (
 )
 
 const Statistics = ({ text, value }) => (
-    <div>{text} {value}</div>
-  )
+  <div>{text} {value}</div>
+)
 
 
 const App = () => {
@@ -16,6 +16,19 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const all = good + neutral + bad
+
+  if (all === 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setGood(good + 1)} text='good' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button handleClick={() => setBad(bad + 1)} text='bad' />
+        <h1>statistics</h1>
+        <div>No feedback given</div>
+      </div>
+    )
+  }
 
   return (
     <div>
