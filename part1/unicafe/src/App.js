021de44897ab819df601cf56c28f1b73bb8088df
Cassiano Caron <cassianocaron@hotmail.com>
2022-02-28 08:32:05 -0300

    Unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,32 +4,43 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad }) => {  
   const all = good + neutral + bad
+  const average = all > 0 ? roundToTwo((good - bad) / all) : 0
+  const positive = all > 0 ? roundToTwo((good / all) * 100) : 0
 
   if (all === 0) {
     return <div>No feedback given</div>
   }
 
   return (
-    <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={(good - bad) / all} />
-      <StatisticLine text='positive' value={(good / all) * 100} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positive + ' %'} />
+      </tbody>
+    </table>
   )
 }
 
 const StatisticLine = ({ text, value }) => (
-  <div>{text} {value}</div>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
+// Round to two decimal places
+const roundToTwo = (num) => (
+  Math.round(num * 100 + Number.EPSILON) / 100
+)
 
 const App = () => {
-  // save clicks of each button to its own state
+  // Save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
