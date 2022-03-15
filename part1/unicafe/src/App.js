baf3d7b32d3c9a431c9361c05219a6054d00b5a7
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-15 11:37:44 -0300

    Format code with prettier code formatter

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,60 +1,58 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
-)
+);
 
-const Statistics = ({ good, neutral, bad }) => {  
-  const all = good + neutral + bad
-  const average = all > 0 ? roundToTwo((good - bad) / all) : 0
-  const positive = all > 0 ? roundToTwo((good / all) * 100) : 0
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = all > 0 ? roundToTwo((good - bad) / all) : 0;
+  const positive = all > 0 ? roundToTwo((good / all) * 100) : 0;
 
   if (all === 0) {
-    return <div>No feedback given</div>
+    return <div>No feedback given</div>;
   }
 
   return (
     <table>
       <tbody>
-        <StatisticLine text='good' value={good} />
-        <StatisticLine text='neutral' value={neutral} />
-        <StatisticLine text='bad' value={bad} />
-        <StatisticLine text='all' value={all} />
-        <StatisticLine text='average' value={average} />
-        <StatisticLine text='positive' value={positive + ' %'} />
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + " %"} />
       </tbody>
     </table>
-  )
-}
+  );
+};
 
 const StatisticLine = ({ text, value }) => (
   <tr>
     <td>{text}</td>
     <td>{value}</td>
   </tr>
-)
+);
 
 // Round to two decimal places
-const roundToTwo = (num) => (
-  Math.round(num * 100 + Number.EPSILON) / 100
-)
+const roundToTwo = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
 
 const App = () => {
   // Save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
