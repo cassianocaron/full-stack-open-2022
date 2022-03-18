8462442cd7ae40bac7ac26c5c370f9813d88af3e
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-18 09:18:32 -0300

    Filter person by name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,16 @@ import { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [personsToShow, setPersonsToShow] = useState(persons);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -16,6 +23,7 @@ const App = () => {
         number: newNumber,
       };
       setPersons(persons.concat(personObject));
+      setPersonsToShow(persons.concat(personObject));
     } else {
       alert(`${newName} is already added to phonebook`);
     }
@@ -31,9 +39,20 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const filterByName = (event) => {
+    const search = event.target.value;
+    setFilter(search);
+    setPersonsToShow(
+      persons.filter((person) => person.name.toLowerCase().includes(search))
+    );
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with <input value={filter} onChange={filterByName} />
+      </p>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -47,8 +66,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
-          <Person key={person.name} person={person} />
+        {personsToShow.map((person) => (
+          <Person key={person.id} person={person} />
         ))}
       </div>
     </div>
