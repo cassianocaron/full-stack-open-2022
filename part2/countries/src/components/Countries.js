e0b8aa11448f65f967b7e36cf3db4446b67c7452
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-22 12:05:16 -0300

    Show country details when button clicked

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import CountryData from "./CountryData";
 
 const Countries = ({ countriesToShow }) => {
+  const [country, setCountry] = useState("");
+
   if (countriesToShow.length === 1) {
     return <CountryData country={countriesToShow[0]} />;
   } else if (countriesToShow.length <= 10) {
     return (
       <div>
         {countriesToShow.map((country) => (
-          <div key={country.name.official}>{country.name.common}</div>
+          <div key={country.name.official}>
+            {country.name.common}{" "}
+            <button onClick={() => setCountry(country)}>show</button>
+          </div>
         ))}
+        {country ? <CountryData country={country} /> : null}
       </div>
     );
   } else if (countriesToShow.length > 10) {
