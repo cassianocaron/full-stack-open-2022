669714a658e45c9807c693a8c3d9a51a8f3282c6
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-23 22:03:39 -0300

    Handle error message - step12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -52,6 +52,11 @@ const App = () => {
             setPersons(updatedPersons);
             setPersonsToShow(updatedPersons);
             setMessage(`Updated ${newPerson.name}`);
+          })
+          .catch((error) => {
+            setMessage(
+              `Information of ${newPerson.name} has already been removed from the server`
+            );
           });
       }
     }
--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,6 +1,10 @@
 const Notification = ({ message }) => {
   if (message === null) return null;
 
+  if (message.includes("Information")) {
+    return <div className="error-message">{message}</div>;
+  }
+
   return <div className="success-message">{message}</div>;
 };
 
