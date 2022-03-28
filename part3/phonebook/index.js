777284b38a4a514153196ae5bffba0ee5191b20d
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-28 15:53:22 -0300

    Delete a single phonebook entry - step4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -48,6 +48,13 @@ app.get("/api/persons/:id", (request, response) => {
   person ? response.json(person) : response.status(404).end();
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
