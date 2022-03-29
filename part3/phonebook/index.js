4126a2821bc1eb8a99eede0ea930578f7dae7d78
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-29 15:13:11 -0300

    Log data sent in HTTP POST requests - step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,14 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("data", (request, response) => {
+  return request.method === "POST" ? JSON.stringify(request.body) : " ";
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
