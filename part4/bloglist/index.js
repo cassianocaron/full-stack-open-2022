cc51a3b23eace7c5bf14ad2a22dee352909292a1
Cassiano Caron <cassianocaron@hotmail.com>
2022-04-08 16:53:03 -0300

    Blog list - step1

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -13,8 +14,15 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = "mongodb://localhost/bloglist";
-mongoose.connect(mongoUrl);
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -33,7 +41,7 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
