f53f8c957dc1f9a878ae5e12a209a8e048766c01
Cassiano Caron <cassianocaron@hotmail.com>
2022-04-11 13:44:09 -0300

    Helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, post) => sum + post.likes, 0);
+};
+
+module.exports = { dummy, totalLikes };
