9cf7214d7d3c958e5336aff413eedc926fbd0f95
Cassiano Caron <cassianocaron@hotmail.com>
2022-05-24 15:11:52 -0300

    Sort blogs by the number of likes - 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -100,9 +100,11 @@ const App = () => {
           <Togglable buttonLabel="new blog" ref={blogFormRef}>
             <BlogForm createBlog={createBlog} />
           </Togglable>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
-          ))}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
+            ))}
         </div>
       )}
     </div>
