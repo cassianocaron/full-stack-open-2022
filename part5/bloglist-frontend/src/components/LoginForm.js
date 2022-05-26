3c3b2d5a33c2169bc237ddcedb0647365e9bb9ef
Cassiano Caron <cassianocaron@hotmail.com>
2022-05-26 13:03:38 -0300

    Define PropTypes and add ESlint config - 5.11-5.12

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const LoginForm = ({ handleLogin }) => {
   const [username, setUsername] = useState("");
@@ -37,6 +38,10 @@ const LoginForm = ({ handleLogin }) => {
       </form>
     </div>
   );
+};
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
 };
 
 export default LoginForm;
--- part5/bloglist-frontend/src/components/Togglable.js
+++ part5/bloglist-frontend/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import { useState, forwardRef, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
 
 const Togglable = forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false);
@@ -32,5 +33,11 @@ const Togglable = forwardRef((props, ref) => {
     </div>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
+
+Togglable.displayName = "Togglable";
 
 export default Togglable;
