0447d5901de446d1cb4cbdd84825bbaae8c81646
Cassiano Caron <cassianocaron@hotmail.com>
2022-06-02 00:33:35 -0300

    Add router for testing

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -30,6 +30,11 @@ app.use("/api/users", usersRouter);
 app.use("/api/blogs", middleware.userExtractor, blogsRouter);
 app.use("/api/login", loginRouter);
 
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
