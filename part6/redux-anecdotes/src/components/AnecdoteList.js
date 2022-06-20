7cc73a69a1a9c75e7bc3b4b03042522d082d8843
Cassiano Caron <cassianocaron@hotmail.com>
2022-06-20 12:05:32 -0300

    Use orderBy from lodash to sort anecdotes

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addVote } from "../reducers/anecdoteReducer";
+import { orderBy } from "lodash";
 
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) => state.anecdotes);
@@ -10,17 +11,17 @@ const AnecdoteList = () => {
     dispatch(addVote(id));
   };
 
-  return [...anecdotes]
-    .sort((a, b) => b.votes - a.votes)
-    .map((anecdote) => (
-      <div key={anecdote.id}>
-        <div>{anecdote.content}</div>
-        <div>
-          has {anecdote.votes}
-          <button onClick={() => handleAddVote(anecdote.id)}>vote</button>
-        </div>
+  const sortedAnecdotes = orderBy(anecdotes, ["votes"], ["desc"]);
+
+  return sortedAnecdotes.map((anecdote) => (
+    <div key={anecdote.id}>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={() => handleAddVote(anecdote.id)}>vote</button>
       </div>
-    ));
+    </div>
+  ));
 };
 
 export default AnecdoteList;
