05fdeb4fd38f9133fee6112fb847d014c54f23a1
Cassiano Caron <cassianocaron@hotmail.com>
2022-07-08 10:43:28 -0300

    Query books of an author - 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -111,7 +111,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      return args.author
+        ? books.filter((book) => book.author === args.author)
+        : books;
+    },
     allAuthors: () => authors,
   },
   Author: {
