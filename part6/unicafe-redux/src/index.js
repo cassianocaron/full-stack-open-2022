6c9e21d09d72b8b68d9345b7ff20fda772eaf3c0
Cassiano Caron <cassianocaron@hotmail.com>
2022-07-25 23:07:23 -0300

    Refactor index.js

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -7,17 +7,15 @@ const store = createStore(reducer);
 
 const App = () => {
   const handleClick = (type) => {
-    return () => {
-      store.dispatch({ type });
-    };
+    store.dispatch({ type });
   };
 
   return (
     <div>
-      <button onClick={handleClick("GOOD")}>good</button>
-      <button onClick={handleClick("OK")}>ok</button>
-      <button onClick={handleClick("BAD")}>bad</button>
-      <button onClick={handleClick("ZERO")}>reset stats</button>
+      <button onClick={() => handleClick("GOOD")}>good</button>
+      <button onClick={() => handleClick("OK")}>ok</button>
+      <button onClick={() => handleClick("BAD")}>bad</button>
+      <button onClick={() => handleClick("ZERO")}>reset stats</button>
       <div>good {store.getState().good}</div>
       <div>ok {store.getState().ok}</div>
       <div>bad {store.getState().bad}</div>
