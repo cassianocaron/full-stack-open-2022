010889bf70b8b4869dc22be818e97633f67c570e
Cassiano Caron <cassianocaron@hotmail.com>
2022-07-30 23:13:49 -0300

    Allow user to input values in the console

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -1,5 +1,31 @@
+const rls = require("readline-sync");
+
+interface Values {
+  value1: number;
+  value2: number;
+}
+
+const height = rls.question("Enter your height in cm: ");
+const weight = rls.question("Enter your weight in kg: ");
+
+const parseInput = (height: string, weight: string): Values => {
+  if (Number(height) <= 0 || Number(weight) <= 0) {
+    throw new Error("Height and weight must be a positive value!");
+  }
+  if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
+    return {
+      value1: Number(height),
+      value2: Number(weight),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2;
+  console.log(`BMI: ${Math.round((bmi + Number.EPSILON) * 100) / 100}`);
+
   if (bmi < 18.5) {
     return "Underweight";
   }
@@ -12,4 +38,13 @@ const calculateBmi = (height: number, weight: number): string => {
   return "Obese";
 };
 
-console.log(calculateBmi(180, 85));
+try {
+  const { value1, value2 } = parseInput(height, weight);
+  console.log(calculateBmi(value1, value2));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
