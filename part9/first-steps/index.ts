bd722b9ca6d607051a222175298f9cc4d19938f8
Cassiano Caron <cassianocaron@hotmail.com>
2022-08-01 13:25:18 -0300

    Add express - 9.4

---
+++ part9/first-steps/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
