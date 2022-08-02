a951e0ee1e681137756ba7f2f3d06ef6b30c36ed
Cassiano Caron <cassianocaron@hotmail.com>
2022-08-01 23:15:18 -0300

    WebBMI - 9.5

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -1,39 +1,39 @@
-import { question } from "readline-sync";
+// import { question } from "readline-sync";
 
-interface rawValues {
-  rawHeight: string;
-  rawWeight: string;
-}
+// interface rawValues {
+//   rawHeight: string;
+//   rawWeight: string;
+// }
 
-interface parsedValues {
-  parsedHeight: number;
-  parsedWeight: number;
-}
+// interface parsedValues {
+//   parsedHeight: number;
+//   parsedWeight: number;
+// }
 
-const getInput = (): rawValues => {
-  const rawHeight = question("Enter your height in cm: ");
-  const rawWeight = question("Enter your weight in kg: ");
+// const getInput = (): rawValues => {
+//   const rawHeight = question("Enter your height in cm: ");
+//   const rawWeight = question("Enter your weight in kg: ");
 
-  return { rawHeight, rawWeight };
-};
+//   return { rawHeight, rawWeight };
+// };
 
-const parseInput = (rawHeight: string, rawWeight: string): parsedValues => {
-  if (Number(rawHeight) <= 0 || Number(rawWeight) <= 0) {
-    throw new Error("Height and weight must be a positive value!");
-  }
-  if (!isNaN(Number(rawHeight)) && !isNaN(Number(rawWeight))) {
-    return {
-      parsedHeight: Number(rawHeight),
-      parsedWeight: Number(rawWeight),
-    };
-  } else {
-    throw new Error("Provided values were not numbers!");
-  }
-};
+// const parseInput = (rawHeight: string, rawWeight: string): parsedValues => {
+//   if (Number(rawHeight) <= 0 || Number(rawWeight) <= 0) {
+//     throw new Error("Height and weight must be a positive value!");
+//   }
+//   if (!isNaN(Number(rawHeight)) && !isNaN(Number(rawWeight))) {
+//     return {
+//       parsedHeight: Number(rawHeight),
+//       parsedWeight: Number(rawWeight),
+//     };
+//   } else {
+//     throw new Error("Provided values were not numbers!");
+//   }
+// };
 
 export const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2;
-  console.log(`BMI: ${Math.round((bmi + Number.EPSILON) * 100) / 100}`);
+  // console.log(`BMI: ${Math.round((bmi + Number.EPSILON) * 100) / 100}`);
 
   if (bmi < 18.5) {
     return "Underweight";
@@ -47,14 +47,17 @@ export const calculateBmi = (height: number, weight: number): string => {
   return "Obese";
 };
 
-try {
-  const { rawHeight, rawWeight } = getInput();
-  const { parsedHeight, parsedWeight } = parseInput(rawHeight, rawWeight);
-  console.log(calculateBmi(parsedHeight, parsedWeight));
-} catch (error: unknown) {
-  let errorMessage = "Something bad happened.";
-  if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
-  }
-  console.log(errorMessage);
-}
+// try {
+//   // const { rawHeight, rawWeight } = getInput();
+//   const { parsedHeight, parsedWeight } = parseInput(
+//     process.argv[2],
+//     process.argv[3]
+//   );
+//   console.log(calculateBmi(parsedHeight, parsedWeight));
+// } catch (error: unknown) {
+//   let errorMessage = "Something bad happened.";
+//   if (error instanceof Error) {
+//     errorMessage += " Error: " + error.message;
+//   }
+//   console.log(errorMessage);
+// }
--- part9/first-steps/index.ts
+++ part9/first-steps/index.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
+
 const app = express();
 
-app.get("/hello", (_req, res) => {
-  res.send("Hello Full Stack!");
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+  const validParameters: boolean =
+    !isNaN(Number(height)) && !isNaN(Number(weight));
+
+  const bmi = calculateBmi(Number(height), Number(weight));
+
+  if (!validParameters || !weight || !height) {
+    res.status(400).send({ error: "malformatted parameters" });
+  }
+
+  res.send({ height, weight, bmi });
 });
 
 const PORT = 3003;
