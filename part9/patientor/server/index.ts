60ff132cb8c93fb0ecbbb35eab3f09836c06f62a
Cassiano Caron <cassianocaron@hotmail.com>
2022-08-05 15:57:49 -0300

    Patientor backend step1 - 9.8

---
+++ part9/patientor/server/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
