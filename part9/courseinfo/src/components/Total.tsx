8ba8e59e0c81c1279b464178ee5ed04707dd28f0
Cassiano Caron <cassianocaron@hotmail.com>
2022-09-15 11:54:54 -0300

    9.14

--- part9/courseinfo/src/components/Total.tsx
+++ part9/courseinfo/src/components/Total.tsx
@@ -1,5 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-const Total = (courseParts: any[]) => {
+interface Parts {
+  name: string;
+  exerciseCount: number;
+}
+
+const Total = (courseParts: Parts[]) => {
   return (
     <p>{courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}</p>
   );
